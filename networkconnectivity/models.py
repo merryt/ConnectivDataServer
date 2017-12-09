cb5c4ddeb376d227a28557ce6e64ac032250e6e7
Tyler Merry <tymmerry@gmail.com>
2017-12-09 12:33:59 -0700

    this kinda works but I am having probblems liking the sections.. Off to do more tutorials

--- networkconnectivity/models.py
+++ networkconnectivity/models.py
@@ -1,23 +1,34 @@
-from django.db import models
+import uuid
 
+from django.db import models
+from django.utils.translation import ugettext_lazy
 
 # Create your models here.
+class UUIDIdMixin(models.Model):
+    class Meta:
+        abstract = True
 
-class NetworkNode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class NetworkNode(UUIDIdMixin):
     node_name = models.CharField(max_length=200)
     mac_address = models.CharField(max_length=17)
     location_of_pi = models.CharField(max_length=200)
     def __str__(self):
         return self.node_name
 
-class NetworkData(models.Model):
-    node_name = models.ForeignKey(NetworkNode, on_delete=models.CASCADE)
+class NetworkData(UUIDIdMixin):
+    node_name = models.ForeignKey(
+        NetworkNode, 
+        editable=False, 
+        verbose_name=ugettext_lazy('networknode'), 
+        related_name='networkdatas',
+        on_delete = models.CASCADE)
     ip_address = models.CharField(max_length=39)
     external_ip = models.CharField(max_length=39)
     timestamp = models.DateTimeField("transmission started")
     ping = models.CharField(max_length=10)
     ping_destination = models.CharField(max_length=200)
-    def __str__(self):
-        return self.timestamp
+
     
     
--- networkconnectivity/views.py
+++ networkconnectivity/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import NetworkNode, NetworkData
+
 
 # Create your views here.
 
@@ -8,7 +8,8 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import UserSerializer, GroupSerializer, NetworkNodeSerializer, NetworkDataSerializer
-# NetworkDataPOSTSerializer
+from .models import NetworkNode, NetworkData
+from django.shortcuts import get_object_or_404
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -40,3 +41,28 @@ class NetworkDataViewSet(viewsets.ModelViewSet):
     """
     queryset = NetworkData.objects.all()
     serializer_class = NetworkDataSerializer
+    
+
+    def get_networknode(self, request, networknode_pk=None):
+        networknode = get_object_or_404(NetworkNode.objects.all(), pk=networknode_pk)    
+        print("asd")
+        return networknode
+
+    def create(self, request, *arg, **kwargs):
+        self.get_networknode(request, networknode_pk=kwargs['node_name'])
+        return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            network_node_pk = self.kwargs['networknode_pk']
+        )
+    # def get_queryset(self):
+    #     print(self)
+    #     return NetworkData.objects.filter(networknode = self.kwargs['networknode_pk'])
+
+    def list(self, request, *arg, **kwargs):
+        self.get_networknode(request, networknode_pk=kwargs['networknode_pk'])
+
+        return super().list(request, *args, **kwargs)
+
+    
